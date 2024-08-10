a0194df0f6bf1eb779be8b2be2509173bdda3075
ShadReza007 <63732556+ShadReza007@users.noreply.github.com>
2024-08-11 01:32:34 +0530

    .

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -4,7 +4,7 @@ import placeholderImage from '../assets/stock-vector-breaking-news-template-with
 
 const NewsItem = ({ title, description, src, url, realProbability, fakeProbability }) => {
   return (
-    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{ maxWidth: "345px", width: "100%", height: "100%" }}>
+    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{ maxWidth: "345px",minHeight:"550px", width: "100%", height: "100%" }}>
       <LazyLoad height={200} offset={100}>
         <img
           src={src ? src : placeholderImage}
@@ -13,8 +13,8 @@ const NewsItem = ({ title, description, src, url, realProbability, fakeProbabili
           alt="..."
         />
       </LazyLoad>
-      <div className="card-body d-flex flex-column justify-content-between" style={{ minHeight: "278px" }}>
-        <h5 className="card-title">{title.slice(0, 50)}</h5>
+      <div className="card-body d-flex flex-column justify-content-between pt-4" style={{ minHeight: "278px" }}>
+        <h5 className="card-title pb-2">{title.slice(0, 50)}</h5>
         <p>
           <span className="badge bg-success">
             Real Probability: {realProbability !== undefined ? (realProbability * 100).toFixed(2) + '%' : 'N/A'}
@@ -24,8 +24,9 @@ const NewsItem = ({ title, description, src, url, realProbability, fakeProbabili
             Fake Probability: {fakeProbability !== undefined ? (fakeProbability * 100).toFixed(2) + '%' : 'N/A'}
           </span>
         </p>
-        <p className="card-text">{description ? description.slice(0, 107)+"..." : "No Description of this event"}</p>
-        <a href={url} className="btn btn-primary mt-auto">Read More</a>
+        <p className="card-text">{description ? description.slice(0, 100)+"..." : "No Description of this event"}</p>
+
+        <a href={url} className="btn btn-primary">Read More</a>
       </div>
     </div>
   );
